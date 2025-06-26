a6b3a10c863c27b6bf0ee47626cdd7c2f902bfeb
Matheus Freitas <mgsoutodelira@gmail.com>
2025-06-25 23:20:47 -0300

    feat: create home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import styles from "./style";
+export default function Home() {
+  return(
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  )
+}
